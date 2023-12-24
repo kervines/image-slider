1b6aeda1524362627ce54b31584762b458b73187
kervines <kervinzero@gmail.com>
2023-12-23 23:12:46 -0300

    update complete project

--- js/script.js
+++ js/script.js
@@ -1,25 +1,30 @@
-const images = document.querySelectorAll('[data-order]');
+const imageSlide = document.querySelector('.slides');
 const next = document.querySelector('.next');
 const previous = document.querySelector('.previous');
 
-images.forEach((img) => {
-  console.log(img);
-});
-
 next.addEventListener('click', nextImage);
 previous.addEventListener('click', previousImage);
+setInterval(nextImage, 5000);
 
+const amountImage = 5; //quantidade de imagens do slide
+const width = imageSlide.clientWidth / amountImage;
 let count = 0;
 function nextImage() {
-  if (count < 4) {
-    images[count].style.marginLeft = -600 + 'px';
-    count++;
+  count += width;
+  imageSlide.style.transform = `translateX(${-count}px)`;
+  if (count > width * 4) {
+    count = 0;
+    imageSlide.style.transform = `translateX(${count}px)`;
   }
 }
 
 function previousImage() {
+  if (count === 0) {
+    count = width * 5;
+    imageSlide.style.transform = `translateX(${count}px)`;
+  }
   if (count > 0) {
-    images[count - 1].style.marginLeft = 0 + 'px';
-    count--;
+    count -= width;
+    imageSlide.style.transform = `translateX(${-count}px)`;
   }
 }
